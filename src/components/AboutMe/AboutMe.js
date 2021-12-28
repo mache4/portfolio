aafee9ea1e82995730f66ae90ab89108690f0e77
mache1 <77922085+mache1@users.noreply.github.com>
2021-12-28 15:35:02 +0100

    about-me resome button changed

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,7 +1,7 @@
 import './AboutMe.scss';
 import Pdf from '../../assets/resume.pdf';
 
-const AboutMe = (props) => {
+const AboutMe = () => {
     return (
         <div className="about-me" id="about-me">
             <h1>About me</h1>
@@ -16,9 +16,8 @@ const AboutMe = (props) => {
             </h3>
             <div className="link-buttons">
                 <button className="linkedin-btn btn" onClick={() => window.open('https://www.linkedin.com/in/branislav-rendulić-9b6411205/')} >LinkedIn</button>
-                <button className="resume-btn btn">
-                    {/* <a className="nav__link" href={Pdf} rel="noreferrer" target="_blank">Resume</a> */}
-                    <a href={Pdf} target="blank">Resume</a>
+                <button onClick={() => window.open(Pdf)} className="resume-btn btn">
+                    Resume
                 </button>
             </div>
         </div>
